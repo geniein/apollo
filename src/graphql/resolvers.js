bc29fcab133bf98ef9305635c15f70efb07df2de
ingenie <cubya@hotmail.co.kr>
2020-04-26 23:25:52 +0900

    init 2

---
+++ src/graphql/resolvers.js
@@ -0,0 +1,25 @@
+import movies from '../database/movies';
+
+const resolvers = {
+    Query: {
+        movies: () => movies,
+        movie: ( _, {id}) =>{
+            return movies.filter(movie => movie.id ===id)[0];
+        }
+    },
+    Mutation: {
+        addMovie: (_, {name, rating}) => {
+            if(movies.find(moive => movie.name === name )) return null;
+
+            const newMovie = {
+                id: movies.length + 1,
+                name,
+                rating
+            };
+            movies.push(newMovie);
+            return newMovie;
+        }
+    }
+};
+
+export default resolvers;
